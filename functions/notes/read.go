c0f8fb1716fffa97cd91841f89c29cff2624cf5a
james <james@scubatribe.com>
2020-11-22 15:18:03 +0100

    Add read note api endpoint

---
+++ functions/notes/read.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"strconv"
+)
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	timestamp, err := strconv.ParseInt(request.PathParameters["timestamp"], 10, 64)
+	item, err := GetNote(request.PathParameters["userId"], timestamp)
+	if err != nil {
+		fmt.Println("Got error fetching record")
+		fmt.Println(err.Error())
+		return events.APIGatewayProxyResponse{Body: "Error", StatusCode: 500}, nil
+	}
+
+	// Log and return result
+	jsonItem, _ := json.Marshal(item)
+	stringItem := string(jsonItem) + "\n"
+	fmt.Println("Retrieved item: ", stringItem)
+	return events.APIGatewayProxyResponse{Body: stringItem, StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
